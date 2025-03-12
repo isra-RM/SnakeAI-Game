4ec75be0776f0a78e613aa829cde94e11b188942
Israel Reyes-Molina <108302899+isra-RM@users.noreply.github.com>
2025-03-12 08:30:03 -0400

    modifying helper

--- helper.py
+++ helper.py
@@ -19,15 +19,15 @@ def plot(scores, mean_scores):
     
     plt.ylim(ymin=0)
     
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]), color='#3498db')
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(round(mean_scores[-1],2)), color='#e74c3c')
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]), color='royalblue')
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(round(mean_scores[-1],2)), color='red')
     
     plt.legend(loc='upper left', fontsize=12, framealpha=0.5)
 
     plt.xticks(fontsize=8)
     plt.yticks(fontsize=8)
     
-    plt.tight_layout()  # Ajusta el layout para que todo quepa bien
+    plt.tight_layout()  
     
     plt.show(block=False)
     plt.pause(.1)
